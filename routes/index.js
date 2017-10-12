14c3b2806d743b7d02bb3810d6f730c2afe89fde
Malik Harrison <harrmalik@gmail.com>
2017-10-12 01:03:05 -0400

    aye

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Express' });
+});
+
 module.exports = router;
